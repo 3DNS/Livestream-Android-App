7c51ceb711083ec73c7ec57307beeba7f8e4c2eb
DoM!niC <info@dom1nic.eu>
2016-09-27 02:27:28 +0200

    added notification

---
+++ app/src/main/java/dom1nic/livestream/PlayerReceiver.java
@@ -0,0 +1,25 @@
+package dom1nic.livestream;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class PlayerReceiver extends BroadcastReceiver {
+    public static final String live = "dom1nic.livestream.LIVE";
+    public static final int MINUTEN = 2;
+
+    public PlayerReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(live)) {
+            context.startService(new Intent(context, PlayerService.class));
+        } else {
+            AlarmManager mgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            mgr.setInexactRepeating(AlarmManager.RTC, System.currentTimeMillis() + 1000, 1000 * 60 * MINUTEN, PendingIntent.getBroadcast(context, 1337, new Intent(live), 0));
+        }
+    }
+}
---
+++ app/src/main/java/dom1nic/livestream/PlayerService.java
@@ -0,0 +1,76 @@
+package dom1nic.livestream;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.v4.content.ContextCompat;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class PlayerService extends Service {
+    public PlayerService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        new AsyncTask<Void, Void, Integer>() {
+            @Override
+            protected Integer doInBackground(Void... params) {
+                try {
+                    HttpURLConnection conn = (HttpURLConnection) new URL("http://dom1nic.eu/viewer/stream.php").openConnection();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    String status = in.readLine();
+                    return Integer.parseInt(status);
+                } catch (Exception e) {
+                }
+                return null;
+            }
+
+            @SuppressLint("NewApi")
+            @Override
+            protected void onPostExecute(Integer integer) {
+                super.onPostExecute(integer);
+                if (integer == null) {
+                    stopSelf();
+                    return;
+                }
+                if (integer != 0) {
+                    NotificationManager mgr = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    Notification.Builder builder = new Notification.Builder(PlayerService.this);
+                    builder.setContentTitle("DoM!niC Livestream");
+                    builder.setContentText("ist jetzt online!");
+                    builder.setContentIntent(PendingIntent.getActivity(PlayerService.this, 7613, new Intent(PlayerService.this, PlayerActivity.class).setData(Uri.parse("http://rtmp.dom1nic.eu:8080/hls/stream/index.m3u8")), 0));
+                    builder.setSmallIcon(android.R.drawable.ic_media_play);
+                    builder.setAutoCancel(true);
+                    builder.setVibrate(new long[]{100, 100, 600, 100});
+                    builder.setLights(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary), 400, 600);
+                    mgr.notify(6725, Build.VERSION.SDK_INT >= 16 ? builder.build() :
+                            builder.getNotification());
+                }
+                stopSelf();
+            }
+        }.execute();
+        return START_NOT_STICKY;
+    }
+}
